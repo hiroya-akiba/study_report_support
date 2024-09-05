80cf8fc75a32b6e2b658189e77260f50d0dc499a
hiroya-akiba <hiroya0504@gmail.com>
2024-09-05 22:21:48 +0900

    時間が空いたので復習

--- views.py
+++ views.py
@@ -1,6 +1,11 @@
 from django.http import HttpResponse
 from django.shortcuts import render
+from django.views.generic import TemplateView
 
 # Create your views here.
 def index(request):
     return HttpResponse("Hello, world. You're at the study report support index.")
+
+# Use Template
+class Index(TemplateView):
+    template_name = "index.html"

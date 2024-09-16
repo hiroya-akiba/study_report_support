e45ccaf23c4aa0263f3c86aea5fd9c42cfa099b9
hiroya-akiba <hiroya0504@gmail.com>
2024-09-17 01:06:05 +0900

    make models (2024/09/16)

--- models.py
+++ models.py
@@ -1,3 +1,21 @@
 from django.db import models
+from django.utils import timezone
 
 # Create your models here.
+class User(models.Model):
+    user_name = models.CharField(max_length=20)
+    is_admin = models.BooleanField(default=False)
+    create_time = models.DateTimeField(default=timezone.now)
+
+class Subject(models.Model):
+    subject_name = models.CharField(max_length=20)
+    create_time = models.DateTimeField(default=timezone.now)
+    update_time = models.DateTimeField(default=timezone.now)
+
+class Report(models.Model):
+    subject = models.ForeignKey(Subject, on_delete=models.CASCADE)
+    time = models.IntegerField(default=0) # 勉強時間：分で記録
+    create_time = models.DateTimeField(default=timezone.now)
+    update_time = models.DateTimeField(default=timezone.now)
+
+

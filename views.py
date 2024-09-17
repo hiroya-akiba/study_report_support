3843839b383a7a64bf7b381e37df02d157593af2
hiroya-akiba <hiroya0504@gmail.com>
2024-09-17 23:06:40 +0900

    2024/09/17 Report表示機能テスト

--- views.py
+++ views.py
@@ -1,11 +1,18 @@
 from django.http import HttpResponse
 from django.shortcuts import render
+from django.template import loader
 from django.views.generic import TemplateView
+from .models import Report
 
 # Create your views here.
 def index(request):
-    return HttpResponse("Hello, world. You're at the study report support index.")
-
-# Use Template
-class Index(TemplateView):
-    template_name = "index.html"
+    # create_dateカラムから5行分
+    latest_report_list = Report.objects.order_by('create_time')[:5]
+    #for in で1行ずつ latestreport を出力して , で繋げる
+    # output = ','.join([rep.subject for rep in latest_report_list])
+    # return HttpResponse(output)
+    template = loader.get_template('study_report_support/index.html')
+    context = {
+        'latest_report_list' : latest_report_list,
+    }
+    return HttpResponse(template.render(context, request))
